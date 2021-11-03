b406ca8dd2050eef531fe252763a9036e4ab9648
Shaunak Basu <basushaunak@msn.com>
2021-11-03 20:57:54 +0530

    Base logic done

--- substring.rb
+++ substring.rb
@@ -1,2 +1,22 @@
 def substring(in_phrase,in_word_list)
+    phrase = in_phrase.downcase.split()
+    words=[]
+    in_word_list.each do |word|
+        words.push(word.downcase())
+    end
+    found = {}
+    words.each do |word| # Individual words in the dictonary
+        phrase.each do |wrd| # Individual words in the phrase
+            if wrd.include?(word) # Dicionary word is present in the word in the phrase
+                if found.dig(word) = nil # New word
+                    found[word] = 1
+                else
+                    found[word]= found[word] + 1 #Existing word
+                end
+            end
+        end
+    end
+    return found
 end
+
+
