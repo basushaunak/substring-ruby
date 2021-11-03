63b0c6f829db28bc1256ecdb86b5e7ba190c50e0
Shaunak Basu <basushaunak@msn.com>
2021-11-03 21:05:25 +0530

    Base logic done

--- substring.rb
+++ substring.rb
@@ -8,7 +8,7 @@ def substring(in_phrase,in_word_list)
     words.each do |word| # Individual words in the dictonary
         phrase.each do |wrd| # Individual words in the phrase
             if wrd.include?(word) # Dicionary word is present in the word in the phrase
-                if found.dig(word) = nil # New word
+                if found.dig(word) == nil # New word
                     found[word] = 1
                 else
                     found[word]= found[word] + 1 #Existing word
@@ -18,5 +18,8 @@ def substring(in_phrase,in_word_list)
     end
     return found
 end
-
-
+ph = "These are the quick and brown quickly foxes and foxens who whose jump over lazy dogs. Yes sire the oxens are pretty lazy"
+dct = ["ouch", "the", "fox", "ox", "oxes", "az"]
+puts "Phrase: #{ph} "
+puts "Dictionary: #{dct}"
+puts substring(ph,dct)
